be7963816d11e12a4c3426a4a7c4d7cd54c5d1aa
Cormac Levins <cormaclevins@outlook.ie>
2019-11-06 15:53:14 +0000

    Fix Heroku/Firebase Creditentials Issue

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -11,7 +11,8 @@ const {
   FIREBASE_APP_ID,
   FIREBASE_MEASUREMENT_ID,
   FIREBASE_PRIVATE_KEY,
-  FIREBASE_CLIENT_EMAIL
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PROJECT_ID_JSON
 } = process.env;
 
 //Create Firebase config
@@ -26,10 +27,18 @@ const firebaseConfig = {
   measurementId: FIREBASE_MEASUREMENT_ID
 };
 
+console.log(FIREBASE_PRIVATE_KEY)
+console.log({
+  "project_id": FIREBASE_PROJECT_ID_JSON.replace(/\\n/g, '\n'),
+  "private_key": FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": FIREBASE_CLIENT_EMAIL.replace(/\\n/g, '\n')
+})
+
+
 //Initialize Firebase admin
 admin.initializeApp({
   credential: admin.credential.cert({
-    "project_id": FIREBASE_PROJECT_ID,
+    "project_id": FIREBASE_PROJECT_ID_JSON,
     "private_key": FIREBASE_PRIVATE_KEY,
     "client_email": FIREBASE_CLIENT_EMAIL
   }),
