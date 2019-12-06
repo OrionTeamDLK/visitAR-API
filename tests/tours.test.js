82b688fb4be729136b759209a928440c72e89083
Cormac Levins <cormaclevins@outlook.ie>
2019-12-06 17:11:49 +0000

    Added Tours Test case 2 & 3

--- tests/tours.test.js
+++ tests/tours.test.js
@@ -1,8 +1,11 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
 const app = require('../src/app')
+
 
 describe('/tours Endpoint', () => {
 
+  //Expected Object 1
   const tourObject1 = {
     id: 1,
     name: "History Tour",
@@ -14,6 +17,7 @@ describe('/tours Endpoint', () => {
     }
   }
 
+  //Expected Object 2
   const tourObject2 = {
     id: 2,
     name: "test tour",
@@ -21,25 +25,47 @@ describe('/tours Endpoint', () => {
     time: 120
   }
 
-  it('When URL is incorrect, Should return 404', async () => {
+  //Test 1
+  it('Test 1: When URL is incorrect, Should return 404', async () => {
     const res = await request(app)
       .get('/tour')
       .expect(404)
   })
 
-  it('When Called, Should return data conataining Correct Values', async () => {
+  //Test 2
+  it('Test 2: When Auth is valid, Should return Correct Data', async () => {
     const res = await request(app)
       .get('/tours')
+      .set('Authorization', 'Bearer ' + jwt.sign({}, process.env.JWT_SECRET) )
+      .expect('Content-Type', /json/)
       .expect(200)
-
-    expect(res.body).toEqual(expect.arrayContaining([
-      expect.objectContaining(tourObject1),
-      expect.objectContaining(tourObject2)
-    ]))
-
   })
+
+  //Test 3
+  it('Test 3: When Auth is Invalid, Should return 401 Error', async () => {
+    const res = await request(app)
+      .get('/tours')
+      .set('Authorization', 'Bearer ' + jwt.sign({}, 'password') )
+      .expect('Content-Type', /json/)
+      .expect(401)
+  })
+
 })
 
 describe('/tourData Endpoint', () => {
+
+  //Test 1
+  it('Test 1: When URL is incorrect, Should return 404', async () => {
+    const res = await request(app)
+      .get('/tourDat')
+      .expect(404)
+  })
+
+  //Test 2
+  it('Test 1: When URL is incorrect, Should return 404', async () => {
+    const res = await request(app)
+      .get('/tourDat')
+      .expect(404)
+  })
 
 })
