c681e0fb2a979a803464743ddcb2f84b1ee2e612
Cormac Levins <cormaclevins@outlook.ie>
2020-01-11 21:22:32 +0000

    User route added, Post user added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const admin = require('./db/firebase.js')
 
 const locationRouter = require('./routers/location')
 const tourRouter = require('./routers/tours')
+const userRouter = require('./routers/user')
 const db = admin.database();
 
 const app = express()
@@ -10,5 +11,6 @@ const app = express()
 app.use(express.json())
 app.use(locationRouter)
 app.use(tourRouter)
+app.use(userRouter)
 
 module.exports = app;
---
+++ src/routers/user.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const router = new express.Router()
+const admin = require('../db/firebase')
+const auth = require('../middleware/auth')
+
+const firestore = admin.firestore()
+
+router.post('/user', auth, async (req, res) => {
+
+  try {
+
+    const uid = req.body.uid;
+
+    const userRecord = await admin.auth().getUser(uid)
+
+    if(userRecord){
+        console.log("Successfully fetched user data:", userRecord.toJSON());
+        const userColRef = firestore.collection('users')
+        let docRefs = await userColRef.listDocuments();
+
+        for( let docRef of docRefs){
+          if (uid == docRef.id) {
+            res.status(400).send({
+              "error": `User ID ${docRef.id} Already Created`
+            })
+            return
+          }
+        }
+
+        const newUser = {
+          "favourite_list": [],
+          "history": []
+        }
+
+        await userColRef.doc(`${uid}`).set(newUser);
+
+    } else {
+      res.status(401).send({ "error" : error });
+      console.log("Error fetching user data:", error);
+    }
+
+    // const profilePicture = (req.body.profile != null) ? req.body.profile :
+    //   'https://firebasestorage.googleapis.com/v0/b/orion-57b76.appspot.com/o/profile_pictures%2Fdeafult_profile.jpg?alt=media&token=dd4a8e95-4dfc-4ce7-bbeb-fcccb64fbf94'
+
+
+
+
+    //new
+
+
+    //old
+    // const newUser = {
+    //   "favourite_list": [],
+    //   "history": [],
+    //   "profile_pic": profilePicture
+    // }
+
+
+
+
+    //add Data
+    //await userColRef.doc(`${uid}`).set(newUser);
+
+    res.status(200).send()
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).send()
+  }
+
+})
+
+module.exports = router
