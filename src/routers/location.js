b9ef1a418d5a68676d5590aa829fd18f2e9acffd
Cormac Levins <cormaclevins@outlook.ie>
2020-02-26 11:30:51 +0000

    GET Locations added

--- src/routers/location.js
+++ src/routers/location.js
@@ -8,33 +8,33 @@ const forecast = require('../utils/forecast');
 
 const firestore = admin.firestore();
 
-// router.get('/locations', async (req, res) => {
-//
-//   try {
-//     const colRef = firestore.collection('LandMarks')
-//     let documentArray = [];
-//
-//     await colRef.listDocuments().then(documentRefs => {
-//       return firestore.getAll(...documentRefs);
-//     }).then(documentSnapshots => {
-//
-//       for (let documentSnapshot of documentSnapshots) {
-//         if (documentSnapshot.exists) {
-//           documentArray.push(documentSnapshot.data())
-//         } else {
-//           console.log(`Found missing document: ${documentSnapshot.id}`);
-//         }
-//       }
-//
-//     });
-//
-//     res.status(200).send(documentArray)
-//
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).send()
-//   }
-// })
+router.get('/locations', auth,  async (req, res) => {
+
+  try {
+    const colRef = firestore.collection('LandMarks')
+    let documentArray = [];
+
+    await colRef.listDocuments().then(documentRefs => {
+      return firestore.getAll(...documentRefs);
+    }).then(documentSnapshots => {
+
+      for (let documentSnapshot of documentSnapshots) {
+        if (documentSnapshot.exists) {
+          documentArray.push(documentSnapshot.data())
+        } else {
+          console.log(`Found missing document: ${documentSnapshot.id}`);
+        }
+      }
+
+    });
+
+    res.status(200).send(documentArray)
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).send()
+  }
+})
 
 router.get('/weather', auth, async(req, res) => {
 
