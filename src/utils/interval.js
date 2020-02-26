ab39cc6418cee93fbb33c157db8260487f1a23be
Cormac Levins <cormaclevins@outlook.ie>
2020-02-26 12:37:46 +0000

    Set Interval time out to 25min

--- src/utils/interval.js
+++ src/utils/interval.js
@@ -1,22 +1,28 @@
 const request = require('request');
 
+rootRequest = () => {
+
+  request({
+      url: "https://orion-visitar.herokuapp.com/",
+      method: "GET",
+      timeout: 10000,
+      followRedirect: true
+  },function(error, response, body){
+      if(!error && response.statusCode == 200){
+          console.log('Request to https://orion-visitar.herokuapp.com/ Successful');
+      }else{
+          console.log('error' + response.statusCode);
+      }
+  });
+
+}
+
+rootRequest();
+
 setIntervalRequest = () => {
-
   var requestLoop = setInterval(function(){
-    request({
-        url: "https://orion-visitar.herokuapp.com/",
-        method: "GET",
-        timeout: 10000,
-        followRedirect: true
-    },function(error, response, body){
-        if(!error && response.statusCode == 200){
-            console.log('Request to https://orion-visitar.herokuapp.com/ Successful');
-        }else{
-            console.log('error' + response.statusCode);
-        }
-    });
-  }, 5000);
-
+    rootRequest();
+  }, 1500000);
 }
 
 module.exports = setIntervalRequest;
