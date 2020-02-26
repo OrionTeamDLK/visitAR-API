eb8756709c578bf21200feb49905a55ba11b565f
Cormac Levins <cormaclevins@outlook.ie>
2020-02-26 12:24:47 +0000

    Add GET '/' Request

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const locationRouter = require('./routers/location')
 const tourRouter = require('./routers/tours')
 const userRouter = require('./routers/user')
 
+const setIntervalRequest = require('./utils/interval');
+
+
 const db = admin.database();
 
 const app = express()
@@ -13,5 +16,13 @@ app.use(express.json())
 app.use(locationRouter)
 app.use(tourRouter)
 app.use(userRouter)
+
+app.get('/', (req, res) => {
+  res.status(200).send("Visit AR")
+})
+
+setIntervalRequest();
+
+
 
 module.exports = app;
